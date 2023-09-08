9a968dd1cf907ac7fc4b3ef2d9ac3fcdca4d0c07
vishnugnair <119111934+vishnugnair@users.noreply.github.com>
2023-09-08 08:49:02 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -5,8 +5,8 @@ export const connectDB = () => {
     .connect(process.env.MONGO_URI, {
       dbName: "6BACKAPI",
     })
-    .then(() => {
-      console.log("Database Connected");
+    .then((c) => {
+      console.log(console.log(`Database connected with ${c.connection.host}`));
     })
     .catch((e) => {
       console.log(e);
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { connectDB } from "./data/database.js";
 
 connectDB();
 
-app.listen(4000, () => {
+app.listen(4005, () => {
   console.log(
     `Server is working in port ${process.env.PORT} in ${process.env.NODE_ENV} Mode`
   );
